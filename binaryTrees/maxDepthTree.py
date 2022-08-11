37820fe7309cdc57005db120f09e109be417077a
ethethanlee <ethethanlee@gmail.com>
2022-08-11 11:50:21 -0400

    binary

---
+++ binaryTrees/maxDepthTree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# works in leetcode lol
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        print(root.left.val)
+        print(root.right.val)
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    
+
+sol = Solution()
+print(sol.maxDepth(root = TreeNode([3,9,20,None,None,15,7])))
+
+
+# BFS for shortest path only pretty much
