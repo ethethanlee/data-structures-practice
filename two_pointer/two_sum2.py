3ab6b97d425748abfc1baa835936077050229949
ethethanlee <ethethanlee@gmail.com>
2022-08-18 16:52:50 -0400

    woo

---
+++ two_pointer/two_sum2.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        p1,p2 = 0,len(numbers)-1
+        while p1<p2:
+            sum = numbers[p1] + numbers[p2]
+            if sum == target:
+                return [p1+1,p2+1]
+            elif sum < target:
+                p1 +=1
+            elif sum > target:
+                p2 -= 1
+        return -1
+
+sol = Solution()
+print(sol.twoSum(numbers = [2,7,11,15], target = 9))
